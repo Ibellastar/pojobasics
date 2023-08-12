3da8818ba725ade41de8fa9593032c87f3bc0338
Isabella Bernal <isabella.bernal.21@tfacorps.org>
2023-08-12 13:17:48 -0500

    first commit

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -13,8 +13,19 @@ keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
 function keysInObject(obj) {
-  
+  let array = []
+  for(let values in obj) {
+  array.push(obj[values])
+  }
+  return array
 }
+
+let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+keysInObject(animals); // => ["dog", "cat", "bison"]
+keysInObject(foods); // => ["apple", "lemon", "mango"]
+
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 module.exports = keysInObject;
