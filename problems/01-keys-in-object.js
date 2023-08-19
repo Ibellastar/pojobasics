fe92c11246d661bbf7c7c4b8284b3a70cfe760fb
Isabella Bernal <isabella.bernal.21@tfacorps.org>
2023-08-19 12:27:25 -0500

    study

--- problems/01-keys-in-object.js
+++ problems/01-keys-in-object.js
@@ -12,19 +12,32 @@ keysInObject(animals); // => ["dog", "cat", "bison"]
 keysInObject(foods); // => ["apple", "lemon", "mango"]
 ***********************************************************************/
 
-function keysInObject(obj) {
-  let array = []
-  for(let values in obj) {
-  array.push(obj[values])
-  }
-  return array
-}
+// function keysInObject(obj) {
+//   let array = []
+//   for(let values in obj) {
+//   array.push(obj[values])
+//   }
+//   return array
+// }
+
+// let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
+// let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
+// keysInObject(animals); // => ["dog", "cat", "bison"]
+// keysInObject(foods); // => ["apple", "lemon", "mango"]
 
 let animals = {dog: 'Wolfie', cat: 'Jet', bison: 'Bilbo'}
 let foods = {apple: 'tart', lemon: 'sour', mango: 'sweet'}
-keysInObject(animals); // => ["dog", "cat", "bison"]
-keysInObject(foods); // => ["apple", "lemon", "mango"]
 
+function keysInObject(obj) {
+  let array = [];
+  for (let key in obj) {
+    array.push(key);  // Use 'key' instead of 'values'
+  }
+  return array;
+}
+
+console.log(keysInObject(animals)); // => ["dog", "cat", "bison"]
+console.log(keysInObject(foods));   // => ["apple", "lemon", "mango"]
 
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
